91b3dafae0314db8d9f01ab837a9a16e3035e93c
Angel Ward <msangelward@gmail.com>
2021-11-09 21:37:56 -0500

    Scaffolded out, created project layers, created Job Class

--- JobSearchOrganizer.Data/IdentityModels.cs
+++ JobSearchOrganizer.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -28,6 +30,36 @@ namespace JobSearchOrganizer.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityuserRoleConfiguration());
+        }
+    }
+    
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    
+    public class IdentityuserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityuserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
---
+++ JobSearchOrganizer.Data/Job.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchOrganizer.Data
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        public string CompanyName { get; set; }
+        [Required]
+        public string JobDescription { get; set; }
+        [Required]
+        public Enum HowApplied { get; set; }
+        public string NextStep { get; set; }
+        [Required]
+        public DateTime DateApplied { get; set; }
+        public string PotentialPointOfContact { get; set; }
+        public DateTime DateOfLastContact { get; set; }
+        public string InterviewNotes { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
