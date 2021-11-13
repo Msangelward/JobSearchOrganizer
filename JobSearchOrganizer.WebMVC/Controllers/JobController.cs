cb7b5b25672f0f69579f1f75255013aa746a6e84
Angel Ward <msangelward@gmail.com>
2021-11-13 09:21:06 -0500

    Created Edit View hid it from User

--- JobSearchOrganizer.WebMVC/Controllers/JobController.cs
+++ JobSearchOrganizer.WebMVC/Controllers/JobController.cs
@@ -75,7 +75,31 @@ namespace JobSearchOrganizer.WebMVC.Controllers
                 };
 
             return View(model);
+        }[HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, JobEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if(model.JobId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateJobService();
+
+            if (service.UpdateJob(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View();
         }
+
+
 
         private JobService CreateJobService()
         {
