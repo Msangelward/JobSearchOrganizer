5736a1c14ddc0284ea2fb705675d3459ceeba4d7
Angel Ward <msangelward@gmail.com>
2021-11-13 08:25:25 -0500

    Updated Create Method with ViewBag & Added Success Method

--- JobSearchOrganizer.WebMVC/Controllers/JobController.cs
+++ JobSearchOrganizer.WebMVC/Controllers/JobController.cs
@@ -1,4 +1,6 @@
 ﻿using JobSearchOrganizer.Models;
+using JobSearchOrganizer.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +15,11 @@ namespace JobSearchOrganizer.WebMVC.Controllers
         // GET: Job
         public ActionResult Index()
         {
-            var model = new JobListItem[0];
-            return View();
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new JobService(userId);
+            var model = service.GetJobs();
+
+            return View(model);
         }
 
         //GET
@@ -25,13 +30,28 @@ namespace JobSearchOrganizer.WebMVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create (JobCreate model)
+        public ActionResult Create(JobCreate model)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) return View(model);
 
-            }
+            var service = CreateJobService();
+
+            if (service.CreateJob(model)) ;
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Note could not be created.");
+
             return View(model);
+        }
+
+        private JobService CreateJobService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new JobService(userId);
+            return service;
         }
     }
 }
