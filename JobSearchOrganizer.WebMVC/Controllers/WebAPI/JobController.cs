22dfdb6d76c62cef8c0d3f82a31a24b711976bf5
Angel Ward <msangelward@gmail.com>
2021-11-16 19:43:13 -0500

    corrected spelling errors

---
+++ JobSearchOrganizer.WebMVC/Controllers/WebAPI/JobController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobSearchOrganizer.WebMVC.Controllers.WebAPI
+{
+    [Authorize]
+    [RoutePrefix("api/job")]
+    public class JobController : ApiController
+    {
+    }
+}
